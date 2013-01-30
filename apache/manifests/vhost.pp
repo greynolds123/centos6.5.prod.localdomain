85f41427f1f1266be095efc71799ba161fdba7d7
Glynn D. Reynolds <glynnreynolds123@gmail.com>
2013-01-30 12:10:25 -0800

    puppet.localdomain(PuppetMaster)

--- apache/manifests/vhost.pp
+++
@@ -1,7 +0,0 @@
-class apache:vhost { 'www.puppet.localdomain':
-       port           => 8140,
-       dcroot         => '/opt/puppet/var/www/www.puppet.localdomain',
-       ssl            => true,
-       priority       => 10,
-       serveraliases  +> 'puppet.localdomain',
-       }
