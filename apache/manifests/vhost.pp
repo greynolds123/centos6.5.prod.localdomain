cf4802461dc9b68b3c2680158daf49d4ced28068
Glynn D. Reynolds <glynnreynolds123@gmail.com>
2013-02-23 06:09:20 -0800

    puppet.localdomain(PuppetMaster)

--- apache/manifests/vhost.pp
+++ apache/manifests/vhost.pp
@@ -1,6 +1,6 @@
 class apache::vhost {
-      file { '/var/www/www.RHEL6.localdomain':
-      dcroot        => '/var/www/www.RHEL6.localdomain',
+      file { '/var/www/www.puppet.localdomain':
+      dcroot        => '/var/www/www.puppet.localdomain',
       port          => 80,
       ssl           => false,
       priority      => 10,
--- apache/manifests/vhostbak.pp
+++ apache/manifests/vhostbak.pp
@@ -1,10 +1,10 @@
 class apache::apache-vhost { 
-      files { '/var/www/www.RHEL6-1.localdomain':
-      dcroot        => '/var/www/www.RHEL6-1.localdomain',
+      files { '/var/www/www.puppet-1.localdomain':
+      dcroot        => '/var/www/www.puppet-1.localdomain',
       port          => 80,
       ssl           => false,
       priority      => 10,
-      serveraliases => 'RHEL6-1.localdomain',
+      serveraliases => 'puppet-1.localdomain',
       }
     }
 
