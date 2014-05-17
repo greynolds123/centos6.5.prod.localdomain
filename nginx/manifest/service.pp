1a0932981351047d08e648792fecde5b7b0259be
Glynn D. Reynolds <greynolds@centos6.5.localdomain>
2014-05-17 13:48:53 -0700

    Modifying Nginx module

---
+++ nginx/manifest/service.pp
@@ -0,0 +1,10 @@
+class nginx::service {
+        service { 'nginx':
+        ensure     => present,
+        hasstatus  => true;
+        hasrestart => true;
+        enable     => true;
+        source     => template(nginx/key.erb);
+        require    => Class['nginx::config'],
+         }
+        }
