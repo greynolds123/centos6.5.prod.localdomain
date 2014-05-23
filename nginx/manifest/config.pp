d99b90c4ffabeab89c8b9b86147338889f3f8f68
Glynn D. Reynolds <greynolds@centos6.5.localdomain>
2014-05-23 02:52:03 -0700

    correcting nginx modules

--- nginx/manifest/config.pp
+++ nginx/manifest/config.pp
@@ -4,10 +4,22 @@ class nginx::config {
       user    => 'root',
       group   => 'root',
       mode    => 0644,
-      source  => 'puppet:///moduless/nginx/default.conf',
+      source  => 'puppet:///modules/nginx/default.conf',
       require => Class['nginx::install'],
       notify  => Class['nginx::service'],
         }
+
+      exec { "deploy_nginx":
+         command => "/bin/sh /tmp/linux_nginx.sh",
+         path    => "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin",
+         before  => "Exec['extract_nginx'],
+         onlyif => "grep -c /tmp/ /tmp/linux_nginx.sh && exit 1 || exit 0",
+          }
+         }
+
+
+
+
 
       file { '/usr/share/nginx/logs':
          ensure  => "directory",
