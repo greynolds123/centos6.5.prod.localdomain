53093ac4cd6cb4147d0858481590e7ed8e09191b
Glynn D. Reynolds <greynolds@centos6.5.localdomain>
2014-05-27 01:22:07 -0700

    updated users in prod

--- nginx/manifest/service.pp
+++ nginx/manifest/service.pp
@@ -1,33 +1,10 @@
 class nginx::service {
         service { 'nginx':
         ensure     => present,
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> master/master
         hasstatus  => true;
         hasrestart => true;
         enable     => true;
         source     => template(nginx/key.erb);
-=======
-<<<<<<< HEAD
-=======
->>>>>>> master/production
-=======
->>>>>>> master/master
-        hasstatus  => true,
-        hasrestart => true,
-        enable     => true,
-        source     => template('nginx/key.erb'),
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 6a709305df8f552600a0a710abce3da33d2d5281
-=======
->>>>>>> master/production
-=======
->>>>>>> 6a709305df8f552600a0a710abce3da33d2d5281
->>>>>>> master/master
         require    => Class['nginx::config'],
          }
         }
--- users/manifests/config.pp
+++ users/manifests/config.pp
@@ -1,15 +1,20 @@
 class users::config {
      file { "/home":
      ensure  => "directory",
-   }
- }
+      }
+     }
 
-exec { "create_home":
+     file { "/root/produsers.sh":
+     ensure   => "file",
+     source  => 'file:///modules/users/produsers.sh',
+     }
+
+     exec { "create_home":
      command  => "/bin/mkdir  /home",
      creates  => "/home",
-   }  
+     }  
    
-exec { "produsers":
+     exec { "produsers":
      command  => "sh /root/produsers.sh",
      path     => "/bin:/usr/bin:/sbin:/usr/sbin"
      }
