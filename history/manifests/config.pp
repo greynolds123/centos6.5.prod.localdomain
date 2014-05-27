4a25506ba135cfb3f3641ad32af13153f30e83d0
Glynn D. Reynolds <greynolds@centos6.5.localdomain>
2014-05-26 17:23:47 -0700

    Updating history

--- history/manifests/config.pp
+++ history/manifests/config.pp
@@ -1,12 +1,21 @@
 class history::config {
-    file { "/etc/profile.d/":
+    file { "/etc/profile.d/history.sh":
     ensure  => present,
     owner   => root,
     group   => root,
-    mode    => "0644",
-    source  => 'puppet:///modules/history/history.conf',
+    mode    =>  0644,
+    source  => template('history/history.conf'),
+     }
     }
-   }
+
+
+exec { "history_copy":
+      command  => "/bin/cp  ../templates/history.sh.erb /etc/profile.d/history.sh",
+      path     => "/bin:/usr/bin:/sbin:/usr/sbin",
+      before   => Exec[history_edit],
+      onlyif   => "grep -c /etc/profile.d/ /etc/profile.d/history.sh && exit 1 || exit 0"
+      }
+
 
 exec { "history_edit":
       command  => "sh /etc/profile.d/history.sh",
