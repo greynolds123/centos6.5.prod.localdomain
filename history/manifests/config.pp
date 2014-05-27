3542dd7026724d6f4f00a540b91eadb2895a0ca6
Glynn D. Reynolds <greynolds@centos6.5.localdomain>
2014-05-26 18:01:11 -0700

    Updating the prod repo

--- history/manifests/config.pp
+++ history/manifests/config.pp
@@ -4,17 +4,9 @@ class history::config {
     owner   => root,
     group   => root,
     mode    =>  0644,
-    source  => template('history/history.conf'),
+    source  => 'puppet:///modules/history/history.sh',
      }
     }
-
-
-exec { "history_copy":
-      command  => "/bin/cp  ../templates/history.sh.erb /etc/profile.d/history.sh",
-      path     => "/bin:/usr/bin:/sbin:/usr/sbin",
-      before   => Exec[history_edit],
-      onlyif   => "grep -c /etc/profile.d/ /etc/profile.d/history.sh && exit 1 || exit 0"
-      }
 
 
 exec { "history_edit":
--- users/manifests/params.pp
+++ users/manifests/params.pp
@@ -2,11 +2,11 @@ class users::params  {
    case $operatingsystem {
 
       /(Ubuntu|Debian)/: {
-       $users_package_name = "produsers"
+       $users_package_name = "exim"
       }
 
       /(RedHat|Fedora)/: {
-       $users_package_name = "produsers"
+       $users_package_name = "exim"
       }
     }
   }     
