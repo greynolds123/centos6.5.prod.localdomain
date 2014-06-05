c9f1f985ff2d3d0c21a28111b755e266e06cacff
Glynn D. Reynolds <greynolds@centos6.5.localdomain>
2014-06-05 12:36:56 -0700

    Updating users module

--- users/manifests/config.pp
+++ users/manifests/config.pp
@@ -9,3 +9,22 @@ class users::config {
      command  => "/bin/mkdir  /home",
      creates  => "/home",
      }   
+
+      file {'/root/prodAddusers.txt':
+      ensure  => present,
+      source  => 'puppet:///modules/users/prodAddusers.txt',
+      }
+
+
+      file {'/root/produsers.sh':,
+      ensure  => present,
+      source  => 'puppet:///modules/users/produsers.sh',
+      }
+
+
+      exec { "produser":
+      command  => "/bin/sh /root/produsers.sh",
+      path     => "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin",
+      onlyif   => "grep -c /root/ /root/produsers.sh && exit 1 || exit 0"
+      }
+
--- users/manifests/users.pp
+++ users/manifests/users.pp
@@ -2,8 +2,8 @@ class users::users {
       user { 'greynolds':
       ensure      => present,
       managehome  => true,
-       }
       }
+      
     
       user { 'fds':
       ensure      => present,
@@ -21,62 +21,33 @@ class users::users {
       managehome => true,
       }
 
-
-
       user { 'mike':
       ensure     => present,
       managehome => true,
       }
-
-
 
       user { 'jill':
       ensure     => present,
       managehome => true,
       }
 
-
       user { 'Anne':
       ensure     => present,
       managehome => true,
       }
-
       
       user { 'Bob':
       ensure     => present,
       managehome => true,
       }
 
-
       user { 'Bill':
       ensure     => present,
       managehome => true,
       }
 
-
       user { 'Jane':
       ensure     => present,
       managehome => true,
       }
-
-
-
-
-       file {'/root/prodAddusers.txt':
-      ensure  => present,
-      source  => 'puppet:///modules/users/prodAddusers.txt',
-      }    
-
-
-      file {'/root/produsers.sh':,
-      ensure  => present,
-      source  => 'puppet:///modules/users/produsers.sh',
-      }
-     
-
-     exec { "produser":
-     command  => "/bin/sh /root/produsers.sh",
-     path     => "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin",
-     onlyif   => "grep -c /root/ /root/produsers.sh && exit 1 || exit 0"
-     }
-
+}
