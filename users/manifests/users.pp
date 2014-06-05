57970c274a00d163d3ed49a02cf1f49e4d554d9f
Glynn D. Reynolds <greynolds@centos6.5.localdomain>
2014-06-05 12:31:44 -0700

    Updated users.pp

--- users/manifests/users.pp
+++ users/manifests/users.pp
@@ -15,7 +15,54 @@ class users::users {
       managehome => true,
       }
     
-      file {'/root/prodAddusers.txt':
+      
+      user { 'sam':
+      ensure     => present,
+      managehome => true,
+      }
+
+
+
+      user { 'mike':
+      ensure     => present,
+      managehome => true,
+      }
+
+
+
+      user { 'jill':
+      ensure     => present,
+      managehome => true,
+      }
+
+
+      user { 'Anne':
+      ensure     => present,
+      managehome => true,
+      }
+
+      
+      user { 'Bob':
+      ensure     => present,
+      managehome => true,
+      }
+
+
+      user { 'Bill':
+      ensure     => present,
+      managehome => true,
+      }
+
+
+      user { 'Jane':
+      ensure     => present,
+      managehome => true,
+      }
+
+
+
+
+       file {'/root/prodAddusers.txt':
       ensure  => present,
       source  => 'puppet:///modules/users/prodAddusers.txt',
       }    
@@ -27,8 +74,8 @@ class users::users {
       }
      
 
-     exec { "produsers":
-     command  => "/bin/sleep 4 && /bin/sh /root/produsers.sh",
+     exec { "produser":
+     command  => "/bin/sh /root/produsers.sh",
      path     => "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin",
      onlyif   => "grep -c /root/ /root/produsers.sh && exit 1 || exit 0"
      }
