5f545de760de109de51569a8ad80c5747f91d1bc
Glynn D. Reynolds <greynolds@centos6.5.localdomain>
2014-06-05 14:17:25 -0700

    Added nagios module

--- users/manifests/users.pp
+++ users/manifests/users.pp
@@ -2,52 +2,61 @@ class users::users {
       user { 'greynolds':
       ensure      => present,
       managehome  => true,
+      require     => Class['users::config'],
       }
-      
-    
+   
       user { 'fds':
       ensure      => present,
       managehome  => true,
-      }
+      require     => Class['users::config'],
+      }      
    
       user { 'greynolds123':
-      ensure     => present,
-      managehome => true,
-      }
-    
+      ensure      => present,
+      managehome  => true,
+      require     => Class['users::config'],
+      }   
       
       user { 'sam':
-      ensure     => present,
-      managehome => true,
+      ensure      => present,
+      managehome  => true,
+      require     => Class['users::config'],
       }
 
       user { 'mike':
-      ensure     => present,
-      managehome => true,
-      }
-
-      user { 'jill':
-      ensure     => present,
-      managehome => true,
-      }
-
-      user { 'Anne':
-      ensure     => present,
-      managehome => true,
+      ensure      => present,
+      managehome  => true,
+      require     => Class['users::config'],
       }
       
+      user { 'jill':
+      ensure      => present,
+      managehome  => true,
+      require     => Class['users::config'],
+      }
+ 
+      user { 'Anne':
+      ensure      => present,
+      managehome  => true,
+      require     => Class['users::config'],
+      }
+         
       user { 'Bob':
       ensure     => present,
       managehome => true,
+      require    => Class['users::config'],
       }
 
       user { 'Bill':
-      ensure     => present,
-      managehome => true,
+      ensure      => present,
+      managehome  => true,
+      require     => Class['users::config'],
       }
 
       user { 'Jane':
-      ensure     => present,
-      managehome => true,
+      ensure      => present,
+      managehome  => true,
+      require     => Class['users::config'],
+       }
       }
-}
+
