4f72a431c8a161fc03cff234e3431df8a5a99912
Glynn D. Reynolds <greynolds@centos6.5.localdomain>
2014-06-13 11:19:45 -0700

    Updating sudo and puppet modules.

--- utilities/manifests/config.pp
+++ utilities/manifests/config.pp
@@ -28,20 +28,20 @@ class utilities::config {
       exec { "run_selinux":
        command  => '/bin/sh /root/manageSelinux.sh',
        path     => '/bin:/usr/bin:/sbin:/usr/sbin',
-       onlyif   => "grep -c /root/ /root/manageSelinux.sh && exit 1 | exit 0"
+       onlyif   => "grep -c /root/ /root/manageSelinux.sh && exit 1 || exit 0"
      }
 
 
       exec { "run_clearcache":
        command  => '/bin/sh /root/clearCache.sh',
        path     => '/bin:/usr/bin:/sbin:/usr/sbin',
-       onlyif   => "grep -c /root/ /root/clearCache.sh && exit 1 | exit 0"
+       onlyif   => "grep -c /root/ /root/clearCache.sh && exit 1 || exit 0"
      }
 
 
       exec { "run_iptables":
        command  => '/bin/sh /root/remoteIPtables.sh',
        path     => '/bin:/usr/bin:/sbin:/usr/sbin',
-       onlyif   => "grep -c /root/ /root/remoteIPtables.sh && exit 1 | exit 0"
+       onlyif   => "grep -c /root/ /root/remoteIPtables.sh && exit 1 || exit 0"
       }
      }
