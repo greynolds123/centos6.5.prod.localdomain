9f68f77e2fe6c39cd569ec620a107c7de54b80a7
Glynn D. Reynolds <greynolds@centos7.prod.localdomain>
2014-08-06 17:03:32 -0700

    Correcting produciton repo

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,57 +3,20 @@ node default {
     if $hostname =~ /^www(\d+)\./ {
     notice("Your modules will deploy to the prod group")
     class { 'history':       }
-<<<<<<< HEAD
-    #class { 'nagios':        }
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    #class { 'nagios':        }
-=======
     class { 'nagios':        }
->>>>>>> b2ee718dd61d28cd8abb19d7056dfb7f81c4ec42
-=======
-    #class { 'nagios':        }
->>>>>>> 18b67f1e00c138b3dee73a70ce811be10d603371
-=======
-    class { 'nagios':        }
->>>>>>> b2ee718dd61d28cd8abb19d7056dfb7f81c4ec42
->>>>>>> 4cd73f9172b56ea1ed268e235245c13e64378eb0
     #class { 'deploy-cobbler':}
     #class { 'bind':          }
     #class { 'mysql':          }
     #class { 'webserver':      }
     class { 'users':          }
-<<<<<<< HEAD
     #class  { 'apache':        }
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    #class  { 'apache':        }
-=======
-    class  { 'apache':        }
->>>>>>> b2ee718dd61d28cd8abb19d7056dfb7f81c4ec42
-=======
-    #class  { 'apache':        }
->>>>>>> 18b67f1e00c138b3dee73a70ce811be10d603371
-=======
-    class  { 'apache':        }
->>>>>>> b2ee718dd61d28cd8abb19d7056dfb7f81c4ec42
->>>>>>> 4cd73f9172b56ea1ed268e235245c13e64378eb0
     #class  { 'dism':          }
     class  { 'f5':            }
     class  { 'java':          }
     #class  { 'mcollective':   }
     class  { 'netapp':        }
-<<<<<<< HEAD
     #class  { 'openstack':     }
     #class  { 'razor':         }
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     #class  { 'openstack':     }
     #class  { 'razor':         }
     #class  { 'splunk':        }
@@ -63,48 +26,12 @@ node default {
     class { 'yum':           }
     #class { 'ntp':           }
     class { 'ldap':          }
-=======
-    class  { 'openstack':     }
-    class  { 'razor':         }
-=======
-    #class  { 'openstack':     }
-    #class  { 'razor':         }
->>>>>>> 18b67f1e00c138b3dee73a70ce811be10d603371
->>>>>>> 4cd73f9172b56ea1ed268e235245c13e64378eb0
-    #class  { 'splunk':        }
-    #class  { 'vmware_lib':    }
-    #class  { 'vmwaretools':   }
-    #class  { 'regiastry':     }
-    class { 'yum':           }
-    #class { 'ntp':           }
-<<<<<<< HEAD
-    class { 'ldap':          }
-=======
-<<<<<<< HEAD
-    #class { 'ldap':          }
->>>>>>> b2ee718dd61d28cd8abb19d7056dfb7f81c4ec42
-=======
-    class { 'ldap':          }
->>>>>>> 18b67f1e00c138b3dee73a70ce811be10d603371
-=======
-    class  { 'openstack':     }
-    class  { 'razor':         }
-    #class  { 'splunk':        }
-    #class  { 'vmware_lib':    }
-    class  { 'vmwaretools':   }
-    #class  { 'regiastry':     }
-    #class { 'yum':           }
-    #class { 'ntp':           }
-    #class { 'ldap':          }
->>>>>>> b2ee718dd61d28cd8abb19d7056dfb7f81c4ec42
->>>>>>> 4cd73f9172b56ea1ed268e235245c13e64378eb0
     #class { 'puppet':        }
     #class { 'selinux':       }
     #class { 'loadbalancer':  }
     class { 'ssh':           }
     class { 'sudo':          }
     class { 'xinetd':        }
-<<<<<<< HEAD
 }   
 Exec { path => '/bin:/usr/bin:/sbin:/usr/sbin' }
 
@@ -112,19 +39,3 @@ Exec { path => '/bin:/usr/bin:/sbin:/usr/sbin' }
    context => '/files/etc/sysctl.conf',
    changes => ['set net.ipv4.ip_forward 1'],
  }
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-}   
-=======
-   
->>>>>>> b2ee718dd61d28cd8abb19d7056dfb7f81c4ec42
-=======
-}   
->>>>>>> 18b67f1e00c138b3dee73a70ce811be10d603371
-=======
-   
->>>>>>> b2ee718dd61d28cd8abb19d7056dfb7f81c4ec42
-Exec { path => '/bin:/usr/bin:/sbin:/usr/sbin' }
->>>>>>> 4cd73f9172b56ea1ed268e235245c13e64378eb0
--- nodes.pp
+++ nodes.pp
@@ -1,7 +1,2 @@
 node default {
 $domain = "centos7.prod.localdomain"}
-<<<<<<< HEAD
-
- 
-=======
->>>>>>> 4cd73f9172b56ea1ed268e235245c13e64378eb0
