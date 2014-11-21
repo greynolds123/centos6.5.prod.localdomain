3b8a687cc8a4e0ccbdb8c2847b55e8aa0c8b603c
Glynn Reynolds <glynnreynolds123@gmail.com>
2014-11-21 04:49:08 -0800

    Update nodes.pp
    Clean up from a recent pull two months ago

--- nodes.pp
+++ nodes.pp
@@ -1,11 +1,6 @@
-node default {
-<<<<<<< HEAD
-$domain = "centos7.prod.localdomain"}
-=======
+node base {
 $domain = 'centos7.prod.localdomain'}
 package {'centos7.prod.localdomain':
   ensure  => 'latest',
   }
 include haproxy
-
->>>>>>> 33066c155e36d3920b86b49b3b83bf3d859f07c8
