acae4d7a6076bc3a202e0ecf73325645b030b11c
Glynn Reynolds <glynnreynolds123@gmail.com>
2014-11-23 00:10:35 -0800

    Update users.pp

--- users/manifests/users.pp
+++ users/manifests/users.pp
@@ -1,6 +1,5 @@
 class users::users {
       user { 'greynolds':
-<<<<<<< HEAD
       ensure      => present,
       managehome  => true,
       require     => Class['users::config'],
@@ -46,53 +45,12 @@ class users::users {
       managehome  => true,
       password    => '$1$nnCRygYZ$/qQeASIdxplrU3h11493J0',
       require     => Class['users::config'],
-=======
-      ensure     => present,
-      managehome => true,
-      require    => Class['users::config'],
-      }
-   
-      user { 'fds':
-      ensure     => present,
-      managehome => true,
-      password   => '$1$nnCRygYZ$/qQeASIdxplrU3h11493J0',
-      require    => Class['users::config'],
-      }
-   
-      user { 'greynolds123':
-      ensure     => present,
-      managehome => true,
-      password   => '$1$nnCRygYZ$/qQeASIdxplrU3h11493J0',
-      require    => Class['users::config'],
-      }
-      
-      user { 'sam':
-      ensure     => present,
-      managehome => true,
-      password   => '$1$nnCRygYZ$/qQeASIdxplrU3h11493J0',
-      require    => Class['users::config'],
-      }
-
-      user { 'mike':
-      ensure     => present,
-      managehome => true,
-      password   => '$1$nnCRygYZ$/qQeASIdxplrU3h11493J0',
-      require    => Class['users::config'],
-      }
       
       user { 'jill':
       ensure     => present,
       managehome => true,
       password   => '$1$nnCRygYZ$/qQeASIdxplrU3h11493J0',
       require    => Class['users::config'],
-      }
- 
-      user { 'Anne':
-      ensure     => present,
-      managehome => true,
-      password   => '$1$nnCRygYZ$/qQeASIdxplrU3h11493J0',
-      require    => Class['users::config'],
->>>>>>> 33066c155e36d3920b86b49b3b83bf3d859f07c8
       }
          
       user { 'Bob':
@@ -103,7 +61,6 @@ class users::users {
       }
 
       user { 'Bill':
-<<<<<<< HEAD
       ensure      => present,
       managehome  => true,
       password    => '$1$nnCRygYZ$/qQeASIdxplrU3h11493J0',
@@ -115,32 +72,11 @@ class users::users {
       managehome  => true,
       password    => '$1$nnCRygYZ$/qQeASIdxplrU3h11493J0',
       require     => Class['users::config'],
-       }
-      }  
-
-      if $user == user { 
-          exec { 'change_password':
-           command   => '/usr/bin/chage -d 0 $HOME/$user',
-             onlyif  => "/bin/grep -c $HOME  $HOME/$user && exit 1 || exit 0",
-=======
-      ensure     => present,
-      managehome => true,
-      password   => '$1$nnCRygYZ$/qQeASIdxplrU3h11493J0',
-      require    => Class['users::config'],
-      }
-
-      user { 'Jane':
-      ensure     => present,
-      managehome => true,
-      password   => '$1$nnCRygYZ$/qQeASIdxplrU3h11493J0',
-      require    => Class['users::config'],
-       }
       }
 
       if $user == user {
           exec { 'change_password':
            command  => '/usr/bin/chage -d 0 $HOME/$user',
-             onlyif => "/bin/grep -c ${HOME}  ${HOME}/${user} && exit 1 || exit 0",
->>>>>>> 33066c155e36d3920b86b49b3b83bf3d859f07c8
+             onlyif => "/bin/grep -c ${HOME}  ${HOME}/${user} && exit 1 || exit 0
       }
      }
