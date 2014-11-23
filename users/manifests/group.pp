f8045e069ffeddb7b34058daf29b9129837e4b44
Glynn Reynolds <glynnreynolds123@gmail.com>
2014-11-23 00:03:43 -0800

    Update group.pp
    Removing duplicartes

--- users/manifests/group.pp
+++ users/manifests/group.pp
@@ -1,9 +1,5 @@
 class users::group {
-<<<<<<< HEAD
-     file { "/etc/group":
-=======
      file { '/etc/group':
->>>>>>> 33066c155e36d3920b86b49b3b83bf3d859f07c8
      ensure  => present,
      require => Class['users::users']
       }
