618ff9fcea910054a59abc6d334cdd2261106272
Glynn Reynolds <glynnreynolds123@gmail.com>
2014-11-22 23:30:15 -0800

    Update config.pp
    Cleared duolicates

--- ssh/manifests/config.pp
+++ ssh/manifests/config.pp
@@ -12,13 +12,7 @@ class ssh::config {
      }
    }
 }
-    if $operatingsystems == '/[Centos|Fedora|Redhat|SLES]/' {
-    file { "/etc/sh/sshd_config":
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0600',
-=======
+    
 class ssh::config {
    file { '/etc/issue.net':
    ensure => present,
@@ -28,61 +22,15 @@ class ssh::config {
    source => 'puppet:///modules/ssh/issue',
    }
   
-    file { '/etc/ssh/ssh_config':
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    source  => 'puppet:///modules/ssh/ssh_config',
-    require => Class['ssh::install'],
-    notify  => Class['ssh::service'],
-   }
     file { '/etc/ssh/sshd_config':
     ensure  => present,
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
->>>>>>> 33066c155e36d3920b86b49b3b83bf3d859f07c8
     source  => 'puppet:///modules/ssh/sshd_config',
     require => Class['ssh::install'],
     notify  => Class['ssh::service'],
-     }
-<<<<<<< HEAD
    }  
-
-class ssh::config {
-    if $operatingsystems == '/[Centos|Fedora|Redhat|SLES]/' {
-    file { "/etc/ssh/ssh_config":  
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0600',
-    source  => 'puppet:///modules/ssh/ssh_config',
-    require => Class['ssh::install'],
-    notify  => Class['ssh::service'],
-     }
-   }
-}
-    if $operatingsystems == '/[Centos|Fedora|Redhat|SLES]/' {
-    file { "/etc/sh/sshd_config":
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0600',
-    source  => 'puppet:///modules/ssh/sshd_config',
-    require => Class['ssh::install'],
-    notify  => Class['ssh::service'],
-     }
-   }  
-
-elsif $operatingsystems == '/[Ubuntu|Debian]/' {
-   file { "/etc/ssh/ssh_config":
-   ensure  => present,
-   owner   => 'root',
-   group   => 'root',
-   mode    => '0600',
-=======
-   }
 
     if $macaddress == '/[00:0C:29:DF:F7:2A]/' {
     file { '/etc/ssh/sshd_config':
@@ -96,37 +44,24 @@ elsif $operatingsystems == '/[Ubuntu|Debian]/' {
      }
     }
 
-
    elsif $operatingsystems == '/[Ubuntu|Debian]/' {
    file { '/etc/ssh/ssh_config':
    ensure  => present,
    owner   => 'root',
    group   => 'root',
    mode    => '0644',
->>>>>>> 33066c155e36d3920b86b49b3b83bf3d859f07c8
    source  => 'puppet:///modules/ssh/ubuntu_ssh_config',
    require => Class['ssh::install'],
    notify  => Class['ssh::service'],
     }
-<<<<<<< HEAD
   }  
-   
-   elsif $operatingsystems == '/[Ubuntu|Debian]/' {
-   file { "/etc/ssh/sshd_config":
-   ensure  => present,
-   owner   => 'root',
-   group   => 'root',
-   mode    => '0600',
-=======
-  }
-   
+  
    elsif $operatingsystems == '/[Ubuntu|Debian]/' {
    file { '/etc/ssh/sshd_config':
    ensure  => present,
    owner   => 'root',
    group   => 'root',
    mode    => '0644',
->>>>>>> 33066c155e36d3920b86b49b3b83bf3d859f07c8
    source  => 'puppet:///modules/ssh/ubuntu_sshd_config',
    require => Class['ssh::install'],
    notify  => Class['ssh::service'],
