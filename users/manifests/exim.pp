b06a710f838223b96d30b174c78f67add9863fb7
Glynn Reynolds <glynnreynolds123@gmail.com>
2014-11-23 00:02:41 -0800

    Update exim.pp
    Removing duplicates

--- users/manifests/exim.pp
+++ users/manifests/exim.pp
@@ -1,6 +1,5 @@
 class users::exim {
      if $operatingsystems == '/[Fedora|Centos|Redhat|SLES]/' {
-<<<<<<< HEAD
      tag("email")
      service { "exim":
      ensure  => running,
@@ -12,19 +11,6 @@ class users::exim {
      service { "exim4":
      ensure  => running,
      enable  => true,
-=======
-     tag('email')
-     service { 'exim':
-     ensure => running,
-     enable => true,
-   }
-  }
-     if $operatingsystems == '/[Ubuntu|Debian]/' {
-     tag('email')
-     service { 'exim4':
-     ensure => running,
-     enable => true,
->>>>>>> 33066c155e36d3920b86b49b3b83bf3d859f07c8
   }
  }
 }
