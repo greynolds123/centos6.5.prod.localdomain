92de9a785d558161d15f30b259bdade3201ad09d
Glynn Reynolds <glynnreynolds123@gmail.com>
2014-11-22 23:35:03 -0800

    Update service.pp
    Removed duplicates

--- ssh/manifests/service.pp
+++ ssh/manifests/service.pp
@@ -1,6 +1,5 @@
 class ssh::service {
       if $operatingsystems == '/[Ubuntu|Debian]/' {
-<<<<<<< HEAD
       service { "/etc/init.d/sshd":
        ensure       => running,
        hasstatus    => true,
@@ -8,20 +7,10 @@ class ssh::service {
        enable       => true,
        source       => 'puppet:///modules/ssh/ubuntu_sshd',
        require      => Class["ssh::config"],
-=======
-      service { '/etc/init.d/sshd':
-       ensure     => running,
-       hasstatus  => true,
-       hasrestart => true,
-       enable     => true,
-       source     => 'puppet:///modules/ssh/ubuntu_sshd',
-       require    => Class['ssh::config'],
->>>>>>> 33066c155e36d3920b86b49b3b83bf3d859f07c8
         }
        }
  
        elsif $operatingsystems == '/[Redhat|Centos|Fedora]/' {
-<<<<<<< HEAD
        service { "/etc/init.d/sshd":
        ensure       => running,
        hasstatus    => true,
@@ -29,15 +18,6 @@ class ssh::service {
        enable       => true,
        source       => 'puppet:///modules/ssh/sshd',
        require      => Class["ssh::config"],
-=======
-       service { '/etc/init.d/sshd':
-       ensure     => running,
-       hasstatus  => true,
-       hasrestart => true,
-       enable     => true,
-       source     => 'puppet:///modules/ssh/sshd',
-       require    => Class['ssh::config'],
->>>>>>> 33066c155e36d3920b86b49b3b83bf3d859f07c8
          }
         }
        }
