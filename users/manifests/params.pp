2fcbfc6ddbcd0a87c518fea13fb5683d7016967a
Glynn Reynolds <glynnreynolds123@gmail.com>
2014-11-23 00:05:31 -0800

    Update params.pp
    Removing duplicates

--- users/manifests/params.pp
+++ users/manifests/params.pp
@@ -1,20 +1,11 @@
 class users::params  {
    case $operatingsystem {
       /(Ubuntu|Debian)/: {
-<<<<<<< HEAD
-       $users_package_name = "exim"
-      }
-      /(RedHat|Fedora)/: {
-       $users_package_name = "exim"
-      }
-    }
-  }     
-=======
-       $users_package_name = 'exim'
+       $users_package_name = "exim4"
       }
       /(RedHat|Fedora)/: {
        $users_package_name = 'exim'
       }
     }
   }
->>>>>>> 33066c155e36d3920b86b49b3b83bf3d859f07c8
+
