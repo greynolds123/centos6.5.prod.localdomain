e8379f58319ae1c6846413a01917aa2fce1893d9
Glynn Reynolds <glynnreynolds123@gmail.com>
2014-11-22 23:39:18 -0800

    Update init.pp
    Removing duplicates

--- sudo/manifests/init.pp
+++ sudo/manifests/init.pp
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 class sudo { 
      include sudo::config, sudo::install, sudo::params
-     }   
-=======
-class sudo {
-     include sudo::config, sudo::install, sudo::params
      }
->>>>>>> 33066c155e36d3920b86b49b3b83bf3d859f07c8
