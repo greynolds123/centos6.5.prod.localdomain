b835b14d44c739beb95709009bd4305443d0803f
Glynn Reynolds <glynnreynolds123@gmail.com>
2014-11-22 23:32:24 -0800

    Update params.pp
    Removed duplicates

--- ssh/manifests/params.pp
+++ ssh/manifests/params.pp
@@ -1,11 +1,5 @@
 class ssh::params {
     case operatingsystem {
-<<<<<<< HEAD
-      Solaris: {
-       $ssh_package_name = 'openssh'
-      }
-=======
->>>>>>> 33066c155e36d3920b86b49b3b83bf3d859f07c8
       /(Ubuntu|Debian)/: {
        $ssh_package_name = 'openssh'
       }
@@ -13,8 +7,3 @@ class ssh::params {
        $ssh_package_name = 'openssh'
       }
    }
-<<<<<<< HEAD
- }      
-=======
- }
->>>>>>> 33066c155e36d3920b86b49b3b83bf3d859f07c8
